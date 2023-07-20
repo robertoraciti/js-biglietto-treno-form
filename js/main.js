707c710284ee7fab1482129a609564bfbaf28c9f
robertoraciti <rob.raciti97@gmail.com>
2023-07-20 18:16:28 +0200

    Uncomplete ex

--- js/main.js
+++ js/main.js
@@ -1,36 +1,38 @@
-const kiloMeters = prompt("Inserisci qui i kilometri che vuoi percorrere");
-const userAge = prompt("Inserisci qui la tua età");
-const unitPrice = 0.21;
-const under = 18;
-const over = 65;
+const userName = document.getElementById("username").value;
 
-parseInt(kiloMeters);
+const kiloMeters = document.getElementById("kilometers").value;
+
+console.log(typeof kiloMeters);
+const userAge = document.getElementById("user-age");
+const unitPrice = 0.21;
+
 parseInt(userAge);
 
-const fullPrice = kiloMeters * unitPrice;
+const basePrice = kiloMeters * unitPrice;
 
-const underDiscount = (fullPrice * 20) / 100;
-const overDiscount = (fullPrice * 40) / 100;
-const underPrice = fullPrice - underDiscount;
+let ticketDiscount = 0;
+console.log(typeof ticketDiscount);
 
-const overPrice = fullPrice - overDiscount;
+const sendButton = document.getElementById("send-button");
 
-if (userAge < under) {
-  let finalPrice = `
-<h3>Il prezzo del tuo biglietto è:</h3>
-<p>€ ${underPrice} `;
+sendButton.addEventListener("click", function () {
+  if (userAge.value === "1") {
+    ticketDiscount = basePrice * 0.2;
+  } else if (userAge.value === "3") {
+    ticketDiscount = basePrice * 0.4;
+  } else if (userAge.value == "2") {
+    ticketDiscount = 0;
+  }
 
-  document.getElementById("final-price").innerHTML = finalPrice;
-} else if (userAge > over) {
-  let finalPrice = `
-  <h3>Il prezzo del tuo biglietto è:</h3>
-  <p>€ ${overPrice} `;
+  const textUserName = (document.getElementById("user-name").innerHTML =
+    userName);
 
-  document.getElementById("final-price").innerHTML = finalPrice;
-} else {
-  let finalPrice = `
-  <h3>Il prezzo del tuo biglietto è:</h3>
-  <p>€ ${fullPrice} `;
+  const textBasePrice = (document.getElementById("base-price").innerHTML =
+    "€ " + basePrice);
 
-  document.getElementById("final-price").innerHTML = finalPrice;
-}
+  const finalPrice = basePrice - ticketDiscount;
+  document.getElementById("final-price").innerHTML = "€ " + finalPrice;
+
+  document.getElementById("discount-applicated").innerHTML =
+    "€ " + ticketDiscount;
+});
